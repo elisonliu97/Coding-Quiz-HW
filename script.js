3aa17428fb8a3a75091d7396bedabe331110e0bf
Elison Liu <elisonliu97@gmail.com>
2021-05-26 15:33:21 -0400

    created function to show questions

--- script.js
+++ script.js
@@ -8,7 +8,40 @@ var resultsEl = document.querySelector("results");
 // VARIABLES
 var timeLeft
 var questionArr
+var qNum
 
+
+// QUESTIONS
+var myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        1: "Douglas Crockford",
+        2: "Sheryl Sandberg",
+        3: "Brendan Eich"
+      },
+      correctAnswer: 2
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        1: "Node.js",
+        2: "TypeScript",
+        3: "npm"
+      },
+      correctAnswer: 2
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        1: "Angular",
+        2: "jQuery",
+        3: "RequireJS",
+        4: "ESLint"
+      },
+      correctAnswer: 3
+    }
+  ];
 
 
 function countdown(){
@@ -17,15 +50,41 @@ function countdown(){
     var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = timeLeft;
+        if(timeLeft < 1) {
+            clearInterval(timeInterval);
+            timerEl.textContent = "Time Left:";
+        }
     }, 1000);
+
 }
 
 function showQuestion(qNum){
+    var qDiv = document.createElement('div');
+    quizEl.appendChild(qDiv);
+    var qPar = document.createElement('p');
+    quizEl.appendChild(qPar);
+
+    qPar.textContent = myQuestions[qNum].question;
+
+    var answerArr = Object.values(myQuestions[qNum].answers);
+    console.log(answerArr)
+
+    for (var ans in answerArr){
+        var qAns = document.createElement("button");
+        qAns.textContent = answerArr[ans];
+        qAns.setAttribute("data-number", ans);
+        quizEl.appendChild(qAns);
+    }
+    
+    
 
 }
 
 function startGame(event){
     countdown();
+    qNum = 0;
+    showQuestion(qNum);
+
 
 }
 
@@ -41,3 +100,7 @@ startEl.addEventListener("click", startGame);
 // COUNTDOWN STARTS
 // QUESTION 1 APPEARS
 // BUTTONS APPEAR
+
+// QUESTIONS AND AnSWERS INSIDE AN ARRAY OF OBJECTS
+// NEED COUNTER TO CHECK WHICH QUESTION IT IS
+// REMEMBER TO CLEAR BUTTONS IF YOU APPEND
