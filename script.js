ce942bc83f6c34f9e2eb6c97abfaf6af2fb4d212
Elison Liu <elisonliu97@gmail.com>
2021-05-26 11:34:19 -0400

    created html elements and referenced them in js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// HTML ELEMENTS
+var timerEl = document.getElementById('timer');
+var startEl = document.getElementById('start');
+var quizEl = document.getElementById('quiz');
+var button = document.querySelector("results");
+
