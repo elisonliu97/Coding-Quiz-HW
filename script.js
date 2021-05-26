17138ce4e078af4bd60dee85c3e56fe42b4ae287
Elison Liu <elisonliu97@gmail.com>
2021-05-26 11:43:42 -0400

    created countdown function in js file

--- script.js
+++ script.js
@@ -2,5 +2,42 @@
 var timerEl = document.getElementById('timer');
 var startEl = document.getElementById('start');
 var quizEl = document.getElementById('quiz');
-var button = document.querySelector("results");
+var resultsEl = document.querySelector("results");
 
+
+// VARIABLES
+var timeLeft
+var questionArr
+
+
+
+function countdown(){
+    timeLeft = 100;
+    timerEl.textContent = timeLeft
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+    }, 1000);
+}
+
+function showQuestion(qNum){
+
+}
+
+function startGame(event){
+    countdown();
+
+}
+
+
+
+
+
+
+// EVENT LISTENERS
+startEl.addEventListener("click", startGame);
+
+// WHEN BUTTON IS CLICKED GAME IS STARTED
+// COUNTDOWN STARTS
+// QUESTION 1 APPEARS
+// BUTTONS APPEAR
